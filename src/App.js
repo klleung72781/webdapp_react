d7ac18d4fc440e2ef9c168afa8128c91b2611063
unknown <klleung72781@gmail.com>
2020-12-07 00:50:51 -0500

    fixing upload

--- src/App.js
+++ src/App.js
@@ -12,92 +12,99 @@ registerPlugin(FilePondPluginImagePreview);
 
 class App extends Component {
 
-  state = {
-    ipfsHash:null,
-    message: 'You haven\'t uploaded any file.',
-    buffer:'',
-    ethAddress:'',
-    blockNumber:'',
-    transactionHash:'',
-    gasUsed:'',
-    txReceipt: ''   
-  };
-
   // const balance = wheels('0x76142ad47eE7e58e1856DFcF1A6996D2C27fb53A');
   // console.log(balance);
-
-  constructor() {
-    super();
-    this.state = { data: [] };
-  }
 
   // getContract() {
   //   const balance = wheels('0x76142ad47eE7e58e1856DFcF1A6996D2C27fb53A');
   //   this.setState({ data: balance });
   // }
 
-  render() {return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.message}
-        </p>
-        <p>
-          {/* {wheels('0x76142ad47eE7e58e1856DFcF1A6996D2C27fb53A')} */}
-        </p>
-      </header>
-      <FilePond
-        server={{
-          process:(fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
-            
-            console.table(file);
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'You haven\'t uploaded any file.',
+      file: [{
+        source: 'index.html',
+        options: {
+            type: 'local'
+        }
+      }],
+      ipfsHash: null
+    };
+  }
 
-            const request = new XMLHttpRequest();
-            request.open('POST', './');
-            // request.setRequestHeader('Content-Type', 'application/json');
-            // request.setRequestHeader('pinata_api_key', '10e2908bf418b0edb269');
-            // request.setRequestHeader('pinata_secret_api_key', '6d5317a95e54d944fe704ea713c12adf7b4099a5a63191a5b3754545ee9bf954');
+  componentDidMount() {
+    // this.timerID = setInterval(
+    //   () => this.tick(),
+    //   1000
+    // );
+  }
 
-            // Should call the progress method to update the progress to 100% before calling load
-            // Setting computable to false switches the loading indicator to infinite mode
-            request.upload.onprogress = (e) => {
-                progress(e.lengthComputable, e.loaded, e.total);
-            };
+  componentWillUnmount() {
+    // clearInterval(this.timerID);
+  }
 
-            // // Should call the load method when done and pass the returned server file id
-            // // this server file id is then used later on when reverting or restoring a file
-            // // so your server knows which file to return without exposing that info to the client
-            // request.onload = function() {
-            //     if (request.status >= 200 && request.status < 300) {
-            //         // the load method accepts either a string (id) or an object
-            //         load(request.responseText);
-            //     }
-            //     else {
-            //         // Can call the error method if something is wrong, should exit after
-            //         error('oh no');
-            //     }
-            // };
-            // console.log(request);
-            // request.send(file);
-            this.ipfsHash = pinFile(file);
-            request.send(file);
-            
-            // Should expose an abort method so the request can be cancelled
-            return {
+  // tick() {
+  //   this.setState({
+  //     date: new Date()
+  //   });
+  // }
+
+  handleInit() {
+  }
+
+  setStateFile(file) {
+    this.setState({
+      file: file
+    });
+    console.log(this.state.file);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.message}
+          </p>
+        </header>
+        <FilePond
+          server={{
+            process:(fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+              
+              this.setStateFile(file);
+
+              const request = new XMLHttpRequest();
+              request.open('POST', './');
+              // Should call the progress method to update the progress to 100% before calling load
+              // Setting computable to false switches the loading indicator to infinite mode
+              request.upload.onprogress = (e) => {
+                  progress(e.lengthComputable, e.loaded, e.total);
+              };
+
+              this.setState({
+                ipfsHash: pinFile(file)
+              });
+              request.send(file);
+              
+              return {
                 abort: () => {
                   // This function is entered if the user has tapped the cancel button
                   request.abort();
 
-                  // Let FilePond know the request has been cancelled
+                  // request cancelled
                   abort();
                 }
-            };
-          }
-        }}
-      ></FilePond>
-    </div>
-  )}
+              };
+            }
+          }}
+          oninit={ () => this.handleInit() }
+        ></FilePond>
+      </div>
+    )
+  }
 };
 
 export default App;
--- src/contracts/wheels.js
+++ src/contracts/wheels.js
@@ -2,17 +2,17 @@ import Fortmatic from 'fortmatic';
 import Web3 from 'web3';
 import wheelsabi from './wheelsabi.json';
 
-async function wheels(address) {
+async function wheels() {
     const fm = new Fortmatic('pk_test_272D468A58C4CA24');
     let web3 = new Web3(fm.getProvider());
 
     const tokenContractInstance = await web3.eth.contract(wheelsabi).at('0x8EBC7785b83506AaA295Bd9174e6A7Ad5681fb80');
-    // const balance = await tokenContractInstance.balanceOf(address).call(function(error, result) {
-    //     if (error) throw error;
-    //     console.log(result);
-    // });
     console.table(tokenContractInstance);
-    // return balance;
+    const totalSupply = tokenContractInstance.totalSupply.call(function(error, result) {
+        if (error) throw error;
+        return result;
+    });
+    return totalSupply;
 }
 
 export default wheels;
